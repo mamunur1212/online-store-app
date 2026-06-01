75cabec412811c026912ff7c65348db58861c5ca
mamun <mamun.ruet.ete19@gmail.com>
2026-06-01 17:40:51 +0200

    creating the profiles and tags tables

---
+++ src/main/resources/db/migration/V3__add_profles_tags.sql
@@ -0,0 +1,26 @@
+CREATE TABLE profiles
+(
+    id             BIGINT PRIMARY KEY,
+    bio            TEXT,
+    phone_number   VARCHAR(15),
+    date_of_birth  DATE,
+    loyalty_points INT UNSIGNED DEFAULT 0,
+    FOREIGN KEY (id) REFERENCES users(id)
+);
+
+-- Create the tags table
+CREATE TABLE tags
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Create the user_tags join table
+CREATE TABLE user_tags
+(
+    user_id BIGINT NOT NULL,
+    tag_id  INT NOT NULL,
+    PRIMARY KEY (user_id, tag_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+);
