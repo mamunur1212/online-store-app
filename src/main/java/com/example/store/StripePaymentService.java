ac45f9dea71bf6a68d865d1bde6087bd91d28036
mamun <mamun.ruet.ete19@gmail.com>
2026-06-01 10:51:23 +0200

    Example of Tight coupling in Dependency Injection

---
+++ src/main/java/com/example/store/StripePaymentService.java
@@ -0,0 +1,8 @@
+package com.example.store;
+
+public class StripePaymentService {
+	public void processPayment(double amount) {
+		System.out.println("STRIPE");
+		System.out.println("Amount " + amount);
+	}
+}
