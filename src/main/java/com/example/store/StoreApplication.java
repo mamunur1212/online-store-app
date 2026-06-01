3732846693bc1a808b51872c286844596964e041
mamun <mamun.ruet.ete19@gmail.com>
2026-06-01 11:16:03 +0200

    Setter injection

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -8,7 +8,8 @@ public class StoreApplication {
 
 public static void main(String[] args) {
 	// SpringApplication.run(StoreApplication.class, args);
-	OrderService orderService = new OrderService(new StripePaymentService());
+	OrderService orderService = new OrderService();
+	orderService.setPaymentService(new StripePaymentService());
 	orderService.placeOrder();
 }
 
