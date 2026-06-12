4f5a6d1c311a7edd94240d21ec7de394483de76f
mamun <mamun.ruet.ete19@gmail.com>
2026-06-12 07:53:24 +0200

    Extracting query parameters

--- src/main/java/com/example/store/controllers/UserController.java
+++ src/main/java/com/example/store/controllers/UserController.java
@@ -5,12 +5,11 @@ import com.example.store.entities.User;
 import com.example.store.mapper.UserMapper;
 import com.example.store.repositories.UserRepository;
 import java.util.List;
+import java.util.Set;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -20,8 +19,12 @@ public class UserController {
   private final UserMapper userMapper;
 
   @GetMapping
-  public List<UserDto> getUsers() {
-    return userRepository.findAll().stream().map(userMapper::toDto).toList();
+  public List<UserDto> getUsers(
+      @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy) {
+    if (!Set.of("name", "email").contains(sortBy)) {
+      sortBy = "name";
+    }
+    return userRepository.findAll(Sort.by(sortBy)).stream().map(userMapper::toDto).toList();
   }
 
   @GetMapping("/{id}")
