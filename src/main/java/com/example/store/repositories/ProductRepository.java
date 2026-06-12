0b3a674a45e110fbc8efd6982367816b78ada274
mamun <mamun.ruet.ete19@gmail.com>
2026-06-12 14:20:08 +0200

    updated EntityGraph for ProductRepository

--- src/main/java/com/example/store/repositories/ProductRepository.java
+++ src/main/java/com/example/store/repositories/ProductRepository.java
@@ -3,8 +3,10 @@ package com.example.store.repositories;
 import com.example.store.entities.Product;
 import java.util.List;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  @EntityGraph(attributePaths = {"category"})
   List<Product> findByCategoryId(Byte categoryId, Sort sort);
 }
