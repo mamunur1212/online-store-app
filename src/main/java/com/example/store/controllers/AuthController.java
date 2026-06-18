6475a6cb052eeee6758413accb8d1655f3f24ebe
mamun <mamun.ruet.ete19@gmail.com>
2026-06-18 11:03:56 +0200

    Added Jwt security

--- src/main/java/com/example/store/controllers/AuthController.java
+++ src/main/java/com/example/store/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.store.controllers;
 
+import com.example.store.dtos.JwtResponse;
 import com.example.store.dtos.LoginRequest;
+import com.example.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
   private final AuthenticationManager authenticationManager;
+  private final JwtService jwtService;
 
   @PostMapping("/login")
-  public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request) {
+  public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request) {
     authenticationManager.authenticate(
         new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
 
-    return ResponseEntity.ok().build();
+    var token = jwtService.generateToken(request.getEmail());
+    return ResponseEntity.ok(new JwtResponse(token));
   }
 }
