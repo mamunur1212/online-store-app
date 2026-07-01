9bdf34061b87da6c872bcc96000c523e4deeee0c
mamun <mamun.ruet.ete19@gmail.com>
2026-07-01 13:19:06 +0200

    Implementing a custom filter to validate JWTs

--- src/main/java/com/example/store/config/SecurityConfig.java
+++ src/main/java/com/example/store/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.store.config;
 
+import com.example.store.filters.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +17,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @AllArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
   private final UserDetailsService userDetailsService;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -53,10 +56,9 @@ public class SecurityConfig {
                     .permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/login")
                     .permitAll()
-                    .requestMatchers(HttpMethod.POST, "/auth/validate")
-                    .permitAll()
                     .anyRequest()
-                    .authenticated());
+                    .authenticated())
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
--- src/main/java/com/example/store/services/JwtService.java
+++ src/main/java/com/example/store/services/JwtService.java
@@ -1,5 +1,6 @@
 package com.example.store.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -25,16 +26,23 @@ public class JwtService {
 
   public boolean validateToken(String token) {
     try {
-      var claims =
-          Jwts.parser()
-              .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
-              .build()
-              .parseSignedClaims(token)
-              .getPayload();
+      var claims = getClaims(token);
 
       return claims.getExpiration().after(new Date());
     } catch (JwtException ex) {
       return false;
     }
+  }
+
+  private Claims getClaims(String token) {
+    return Jwts.parser()
+        .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  public String getEmailFromToken(String token) {
+    return getClaims(token).getSubject();
   }
 }
